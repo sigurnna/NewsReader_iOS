bbc45b76c4e9aca9fd63c33a664b2d70bd253dd8
seyeon <seyeon@futurewiz.co.kr>
2018-09-05 21:43:38 +0900

    initial commit

---
+++ NewsReader/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  NewsReader
+//
+//  Created by 이승준 on 2018. 9. 5..
+//  Copyright © 2018년 seungjun. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
