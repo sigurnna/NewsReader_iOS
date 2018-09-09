dd1dfd11d75b1430bcbecdc19408980cccea6450
sigurnna <sigurnna@futurewiz.co.kr>
2018-09-09 15:09:11 +0900

    - 기획서 업데이트 및 초기 파일 추가.

---
+++ NewsReader/Presentation/Main/PageContentViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PageContentViewController.swift
+//  NewsReader
+//
+//  Created by 이승준 on 2018. 9. 9..
+//  Copyright © 2018년 seungjun. All rights reserved.
+//
+
+import UIKit
+
+/// MainPageViewController에 viewControllers로 추가되는 하위 VC.
+/// 각 페이지의 Contents를 담당함.
+class PageContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
